2d38f4f04630356f2d083f853a3f2163199cda5d
JurgenOS <8andal@mail.ru>
2018-10-12 02:08:06 +0000

    Changes to be committed: 	modified:   test.py

--- part_2/test.py
+++ part_2/test.py
@@ -1,20 +1,32 @@
 import getpass
 import telnetlib
 import sys
+import time
 
-user = input("Enter your remote account: ")
-password = getpass.getpass()
+#user = input("Enter your remote account: ")
+user = "jos"
+#password = getpass.getpass()
+password = "Boroda_11"
 
 for host in range (200, 203):
 
     tn = telnetlib.Telnet("192.168.206.{}".format(host))
+    time.sleep(2)
     tn.read_until(b"Username: ")
-    tn.write(user.encode('ascii') + b"\n")
+    time.sleep(2)
+    tn.write(b"jos\n")
 
     if password:
+        time.sleep(2)
         tn.read_until(b"Password: ")
-        tn.write(password.encode('ascii') + b"\n")
+        tn.write(b"Boroda_11\n")
 
+    tn.read_very_eager()
+    time.sleep(2)
+    tn.expect(b".*[>#]")
+    tn.write(b"\n")
     tn.write(b"sh ip int brie\n")
-    
-    print(tn.read_all().decode('ascii'))
+    tn.write(b"end\n")
+    tn.write(b"exit\n")
+
+    print(tn.read_very_eager())
