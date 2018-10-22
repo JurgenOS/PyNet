c6c21034702c6984bd73a2fcd3d3c8c6c35c7731
JurgenOS <32762406+JurgenOS@users.noreply.github.com>
2018-10-22 07:52:03 +0300

    telnet for py 2 and py 3
    the function sends command through python 2 or pyhton 3

---
+++ part_2/telnet_compat.py
@@ -0,0 +1,35 @@
+import telnetlib
+import time
+
+
+def send_command_telnet(hosts, command, user, password):
+    # hosts - the list of hosts
+    # commad - the list of command
+    # the function returns 'tn.read_all()' output
+
+    for node in hosts:
+        tn = telnetlib.Telnet(node)
+    
+        tn.read_until(b"Username: ")
+        tn.write(user.encode("utf-8") + b"\n")
+        if password:
+            tn.read_until(b"Password: ")
+            tn.write(password.encode("utf-8") + b"\n")
+    
+        for item in  command:
+            tn.write(item.encode("utf-8") + b"\n")
+            time.sleep(2)
+     
+        res = tn.read_all()
+        tn.close()
+
+        return res
+
+
+if __name__ == "__main__":
+    user = "jos"
+    password = "Boroda_11"
+    hosts = ["192.168.73.200"]
+    command = [" show version", " exit"]
+    
+    print(send_command_telnet(hosts, command, user, password))
